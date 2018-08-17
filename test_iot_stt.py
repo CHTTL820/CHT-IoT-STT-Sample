43a22fdbbc994e908c0cbeee77febe3cc88fb6c0
CHTTL820 <38549157+CHTTL820@users.noreply.github.com>
2018-08-17 17:16:47 +0800

    Update test_iot_stt.py
    update new stt sample

--- test_iot_stt.py
+++ test_iot_stt.py
@@ -1,6 +1,6 @@
 # encoding=UTF-8
 import logging
-import _thread
+import thread
 import time
 import requests
 import json
@@ -9,27 +9,20 @@ logging.basicConfig(level=logging.DEBUG, format="%(message)s")
 
 def run():
     lens = 0
-    file = open('TestInput0.raw', 'rb')
+    file = open('今天天氣很好.raw', 'rb')
     DataBuffer = file.read()
     lens = len(DataBuffer)
     
     gStartTime = time.time()
     #步驟一:設定參數
     header = {
-      'Action': 'connect',
-      'IP': '127.0.0.1',
-      'AsrType': 'AsrAgent',
-      'ClientName': 'AssistantTest',
-      'MaxCandidateNum': '3',
-      'AsrEngName': 'TLAsrSrv_SttDnnKd',
-      'SpeechFormat': '0',
-      'AsrGrmNum': '0',
-      'AsrGrmName0': ''
+    'Action':'connect',
+    'ClientId':'Test' #區別每個使用者的unique id, 請自行定義
     }
     
     #步驟二:開始連線，因為要用http 1.1 keep-alive python必須用session
     session = requests.Session()
-    res = session.post('http://iot.cht.com.tw/api/chtlasr/MyServlet/tlasr', params=header,data="")
+    res = session.post('http://iot.cht.com.tw/api/chtlasr/MyServlet/tlasr', params=header, data="")
 
     #步驟三:取得ASR Reference Id, 用來告訴server這些語音buffer是同一次辨識
     handle = str(res.text).split(';')[0]
@@ -37,7 +30,7 @@ def run():
     if str(res.text).find('fail') == -1: 
         j = 0
         while j < lens:
-            bytessend = 9600
+            bytessend = 4800
             if(j + bytessend > lens):
                 bytessend = lens - j
                 
@@ -52,10 +45,15 @@ def run():
             res = session.post('http://iot.cht.com.tw/api/chtlasr/MyServlet/tlasr', params=header, data=DataBuffer[j:j+bytessend])
             j = j + bytessend
 
+            if(time.time()-gSyncTime) > 0.15:
+                print ' '
+            else:#模擬streaming,等時間到再送
+                time.sleep(0.15-(time.time()-gSyncTime))
             print (" get text from server %s, %f\n" % (res.text, time.time() - gStartTime))
-            #Server通知client, 切到語音，有辨識結果，可以停止送語音過來
-            if str(res.text).find('Speech Got') != -1:
-                #client通知Server停送語音。另一種情形，如果Server還沒切到音，client要主動停止，也請送SpeechEnd通知Server，取回目前辨識結果。
+            #Server通知client, 切到語音，有辨識結果，可以停止送語音過來，並取得辨識結果
+            #client通知Server停送語音。另一種情形，如果Server還沒切到音，client要主動停止，也請送SpeechEnd通知Server，取回目前辨識結果。
+            #if str(res.text).find('result') != -1 or (j == lens):
+            if str(res.text.encode('utf-8')).find('result') != -1 or (j == lens):
                 header = {
                     'Action':'syncData',
                     'AsrReferenceId':handle,
@@ -63,15 +61,16 @@ def run():
                     'SpeechEnd':'y'
                 }
                 res = session.post('http://iot.cht.com.tw/api/chtlasr/MyServlet/tlasr', params=header, data="")
-                print (" final %s, %f\n" % (res.text, time.time() - gStartTime))
+                if str(res.text.encode('utf-8')).find('fail') != -1:
+                    print (" No result...")
+                else:
+                    print (" final %s, %f\n" % (res.text, time.time() - gStartTime))
                 j = lens+100
                 break
-
-        #步驟五:跟server要辨識結果
-        header = {'AsrReferenceId':handle}
-        res = session.get('http://iot.cht.com.tw/api/chtlasr/MyServlet/tlasr', params=header)
-        print ("%s = %f\n" % (res.text,time.time() - gStartTime))
+            
+        #步驟五:可以再跟server要辨識結果
+        #header = {'AsrReferenceId':handle}
+        #res = session.get('http://iot.cht.com.tw/api/chtlasr/MyServlet/tlasr', params=header)
+        #print ("%s = %f\n" % (res.text,time.time() - gStartTime))
     
 run()
-
-
